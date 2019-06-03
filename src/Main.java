65a635ba38098e2a1e13331290a7287491a3ae18
Grzegorz <grzegorzgolan18@gmail.com>
2019-06-03 12:14:45 +0200

    v3

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        suma();
-
-    }
-
-    public static void suma(){
         int number;
-        int sum = 0;
         Scanner scan = new Scanner(System.in);
         System.out.println("wpisz liczbe całkowitą a obliczę sumę ich cyfr");
         number = scan.nextInt();
+        System.out.println(suma(number)); ;
+
+    }
+
+    public static int suma(int number){
+
+        int sum = 0;
+
+
 
         while (number != 0) {
 
             sum = sum + number % 10;
             number = number / 10;
         }
-        System.out.println(sum);
 
+
+    return sum;
     }
 }
